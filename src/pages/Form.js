40105470f800e311ff4109963a0e6ad4ca72160a
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-09-08 10:52:47 -0400

    miligram;

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Form = (props) => {
-  return <h1>Form</h1>;
+const Form = ({initalTodo, handleSubmit, buttonLabel, history}) => {
+
+  // The form data state
+  const [formData, setFormData] = useState(initalTodo)
+
+  // functions
+  const handleChange = (event) => {
+    setFormData({...formData, [event.target.name]: event.target.value})
+  }
+  const handleSubmission = (event) => {
+    event.preventDefault()
+  }
+
+  return <form onSubmit={handleSubmit} >
+    <input
+    type="text"
+    onChange={handleChange}
+    v
+    />
+    </form>
 };
 
 export default Form;
